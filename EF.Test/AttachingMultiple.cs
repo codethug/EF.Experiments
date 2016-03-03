5090c6d035c84e87fb2837fb91ccef8040131dc5
codethug <tlarbo@gmail.com>
2016-03-03 13:55:07 -0500

    Preventing parent records from being readded to database

--- EF.Test/AttachingMultiple.cs
+++ EF.Test/AttachingMultiple.cs
@@ -6,6 +6,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EF.Data.Context;
 using System;
 using FluentAssertions;
+using System.Collections.Generic;
+using EF.Test.Helpers;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace EF.Test
 {
@@ -55,9 +59,12 @@ namespace EF.Test
             var createdParent1 = context.CreateParent("Susan");
             Child createdChild = context.CreateChild("Sam", createdParent1, createdParent1);
             context.SaveChanges();
+            var numParentsBefore = context.Parents.Count();
             // Remove items from EF cache
             context.Entry(createdParent1).State = EntityState.Detached;
             context.Entry(createdChild).State = EntityState.Detached;
+            // Log all commands sent to the database
+            var dbCommands = new StringBuilder();
 
             // Act
             var child = context.Children
@@ -70,13 +77,31 @@ namespace EF.Test
 
             context.Children.Add(child);
             context.Entry(child).State = EntityState.Modified;
+            // Prevent parents from being re-added to database
+            context.Entry(child.Parent1).State = EntityState.Detached;
+            context.Entry(child.Parent2).State = EntityState.Detached;
+            // Start logging commands
+            context.Database.Log = (command) => dbCommands.AppendLine(command);
             context.SaveChanges();
             // Remove the child from EF's cache
             context.Entry(child).State = EntityState.Detached;
+            // Stop logging commands
+            context.Database.Log = null;
+            var sql = dbCommands.ToString();
 
             // Assert
+            // Verify that Updates were only done to Children
+            var numUpdates = sql.CountSubstrings("UPDATE");
+            var numChildrenUpdates = sql.CountSubstrings("UPDATE [dbo].[Children]");
+            numUpdates.Should().Be(numChildrenUpdates);
+            // Verify that no Inserts were done
+            sql.CountSubstrings("INSERT").Should().Be(0);
+            // Verify the child record was updated
             var assertChild = context.Children.Single(c => c.Id == createdChild.Id);
             assertChild.Name.Should().Be("New Child Name");
+            // Verify no parents were added
+            var numParentsAfter = context.Parents.Count();
+            numParentsBefore.Should().Be(numParentsAfter);
         }
 
     }
---
+++ EF.Test/Helpers/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EF.Test.Helpers
+{
+    public static class StringExtensions
+    {
+        public static int CountSubstrings(this string input, string textToFind)
+        {
+            return Regex.Matches(input, Regex.Escape(textToFind)).Count;
+        }
+    }
+}
